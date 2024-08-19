3a52ad94ffc145b6ff95e5c0c8c845a11fc646e2
Rakibul <rakibulislam8226@gmail.com>
2024-08-19 16:23:14 +0600

    Setting changes for get env values

--- src/config/settings.py
+++ src/config/settings.py
@@ -215,9 +215,9 @@ AUTHENTICATION_BACKENDS = [
 
 
 # Celery settings with redis as broker
-CELERY_BACKEND = os.environ["CELERY_BACKEND"]
-CELERY_BROKER_URL = os.environ["CELERY_BROKER_URL"]
-CELERY_RESULT_BACKEND = os.environ["CELERY_RESULT_BACKEND"]
+CELERY_BACKEND = os.getenv("CELERY_BACKEND", "redis://127.0.0.1:6379/3")
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/1")
 
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
